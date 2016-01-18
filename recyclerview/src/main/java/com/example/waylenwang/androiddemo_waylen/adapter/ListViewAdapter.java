2f07be87fb8e5f5a1c1ddc6df4c8701d76d0baae
wangxianglei <2841652096@qq.com>
2016-01-18 18:09:56 +0800

    update recyclerViewDemo

--- recyclerview/src/main/java/com/example/waylenwang/androiddemo_waylen/adapter/ListViewAdapter.java
+++ recyclerview/src/main/java/com/example/waylenwang/androiddemo_waylen/adapter/ListViewAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.waylenwang.androiddemo_waylen.R;
 
@@ -44,9 +45,15 @@ public class ListViewAdapter extends RecyclerView.Adapter<ListViewAdapter.MyView
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
 
         holder.tv.setText(list.get(position));
+        holder.tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,""+position,Toast.LENGTH_SHORT);
+            }
+        });
 
 
     }
